0f08f93183f0ba955a620f90e9d7b7a4db1eff5a
Nagy Krisztián <knagy@deadlime.hu>
2012-06-24 20:08:11 +0200

    Fixed #1

--- README.md
+++ README.md
@@ -3,13 +3,17 @@ Todo.txt for Sublime Text 2
 
 Todo.txt syntax highlighting and keyboard shortcuts. [About todo.txt](http://todotxt.com/).
 
-- `Ctrl+K` `Ctrl+R`  
+- `Ctrl+K` `Ctrl+R`
   Remove priority from task
-- `Ctrl+K` `Ctrl+H`  
+- `Ctrl+K` `Ctrl+H`
   Set task priority to high
-- `Ctrl+K` `Ctrl+M`  
+- `Ctrl+K` `Ctrl+M`
   Set task priority to medium
-- `Ctrl+K` `Ctrl+L`  
+- `Ctrl+K` `Ctrl+L`
   Set task priority to low
-- `Ctrl+K` `Ctrl+D`  
+- `Ctrl+K` `Ctrl+D`
   Toggle done/undone
+- `Ctrl+Alt+Keypad Plus`
+  Increase priority
+- `Ctrl+Alt+Keypad Minus`
+  Decrease priority
--- set_priority.py
+++ set_priority.py
@@ -5,7 +5,7 @@ class SetPriorityCommand(sublime_plugin.TextCommand):
 		for region in self.view.sel():
 			line = self.view.line(region)
 			
-			result = re.match(r"^\([ABC]\) ", self.view.substr(line))
+			result = re.match(r"^\([A-Z]\) ", self.view.substr(line))
 			if result:
 				if priority:
 					self.view.replace(edit, sublime.Region(line.begin(), line.begin() + 3), priority)
@@ -13,3 +13,29 @@ class SetPriorityCommand(sublime_plugin.TextCommand):
 					self.view.erase(edit, sublime.Region(line.begin(), line.begin() + 4))
 			elif priority:
 				self.view.insert(edit, line.begin(), priority + " ")
+
+class IncreasePriorityCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			line = self.view.line(region)
+			
+			result = re.match(r"^\(([A-Z])\) ", self.view.substr(line))
+			if result:
+				letter = result.group(1)
+				if letter != "A":
+					self.view.replace(edit, sublime.Region(line.begin(), line.begin() + 3), "(" + chr(ord(letter) - 1) + ")")
+			else:
+				self.view.insert(edit, line.begin(), "(Z) ")
+
+class DecreasePriorityCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			line = self.view.line(region)
+			
+			result = re.match(r"^\(([A-Z])\) ", self.view.substr(line))
+			if result:
+				letter = result.group(1)
+				if letter != "Z":
+					self.view.replace(edit, sublime.Region(line.begin(), line.begin() + 3), "(" + chr(ord(letter) + 1) + ")")
+				else:
+					self.view.erase(edit, sublime.Region(line.begin(), line.begin() + 4))
